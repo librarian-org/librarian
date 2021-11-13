3f00c181d97ee98fe9cc8ece95c24c326601520e
Danilo Lutz <danilolutz@protonmail.ch>
2021-11-13 15:39:38 -0300

    feat: added librarian icons (#4)

--- forge.config.js
+++ forge.config.js
@@ -1,14 +1,56 @@
 module.exports = {
-    publishers: [
+  packagerConfig: {
+    icon: `${__dirname}/src/assets/images/librarian`,
+    appCopyright: "Danilo Lutz"
+  },
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "librarian",
+        iconUrl: `file:///${__dirname}/src/assets/images/librarian.ico`,
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        name: "librarian",
+        iconUrl: `file:///${__dirname}/src/assets/images/librarian.png`,
+      },
+    },
+  ],
+  plugins: [
+    [
+      "@electron-forge/plugin-webpack",
       {
-        name: '@electron-forge/publisher-github',
-        config: {
-          repository: {
-            owner: 'danilolutz',
-            name: 'librarian'
-          },
-          prerelease: true
-        }
-      }
-    ]
-  }
+        mainConfig: "./webpack.main.config.js",
+        renderer: {
+          config: "./webpack.renderer.config.js",
+          entryPoints: [
+            {
+              html: "./src/index.html",
+              js: "./src/renderer.ts",
+              name: "main_window",
+            },
+          ],
+        },
+      },
+    ],
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "danilolutz",
+          name: "librarian",
+        },
+        prerelease: true,
+      },
+    },
+  ],
+};
