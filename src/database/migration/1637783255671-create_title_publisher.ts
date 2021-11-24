b8cc4b4860d83f643fde3d082d262bfd0ed5e79f
André Gava <andrelzgava@gmail.com>
2021-11-24 17:12:44 -0300

    fix: some fiels where forgotten

--- src/database/migration/1637783255671-create_title_publisher.ts
+++ src/database/migration/1637783255671-create_title_publisher.ts
@@ -1,57 +1,81 @@
-import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
 
 export class createTitlePublisher1637783255671 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'title_publisher',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'classification',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'edition',
+            type: 'int',
+          },
+          {
+            name: 'published_at',
+            type: 'varchar',
+          },
+          {
+            name: 'title_id',
+            type: 'int',
+          },
+          {
+            name: 'publisher_id',
+            type: 'int',
+          },
+        ],
+      }),
+      true
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table({
-            name: "title_publisher",
-            columns: [
-                {
-                    name: "id", 
-                    type: "int",
-                    isPrimary: true
-                },
-                {
-                    name: "info",
-                    type: "varchar",
-                },
-                {
-                    name: "title_id", 
-                    type: "int",
-                },
-                {
-                    name: "publisher_id", 
-                    type: "int",
-                },
+    await queryRunner.createForeignKey(
+      'title_publisher',
+      new TableForeignKey({
+        columnNames: ['title_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'title',
+        onDelete: 'CASCADE',
+      })
+    );
 
-            ]
-        }), true)
+    await queryRunner.createForeignKey(
+      'title_publisher',
+      new TableForeignKey({
+        columnNames: ['publisher_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'publisher',
+        onDelete: 'CASCADE',
+      })
+    );
+  }
 
-        await queryRunner.createForeignKey("title_publisher", new TableForeignKey({
-            columnNames: ["title_id"],
-            referencedColumnNames: ["id"],
-            referencedTableName: "title",
-            onDelete: "CASCADE"
-        }));
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('title_publisher');
 
-        await queryRunner.createForeignKey("title_publisher", new TableForeignKey({
-            columnNames: ["publisher_id"],
-            referencedColumnNames: ["id"],
-            referencedTableName: "publisher",
-            onDelete: "CASCADE"
-        }));
+    const titleId = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('title_id') !== -1
+    );
+    await queryRunner.dropForeignKey('title_publisher', titleId);
 
-    }
+    const publisherId = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('publisher_id') !== -1
+    );
+    await queryRunner.dropForeignKey('title_publisher', publisherId);
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        const table = await queryRunner.getTable("title_publisher");
-
-        const titleId = table.foreignKeys.find(fk => fk.columnNames.indexOf("title_id") !== -1);
-        await queryRunner.dropForeignKey("title_publisher", titleId);
-
-        const publisherId = table.foreignKeys.find(fk => fk.columnNames.indexOf("publisher_id") !== -1);
-        await queryRunner.dropForeignKey("title_publisher", publisherId);
-
-        await queryRunner.dropTable("title_publisher");
-    }
+    await queryRunner.dropTable('title_publisher');
+  }
 }
