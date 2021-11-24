4f97d9ee0627ed464f7a5ff6a961e1fd144b0697
André Luiz Gava <andrelzgava@gmail.com>
2021-11-24 13:31:04 -0300

    feat: create model category #31 (#32)
    * feat: create model category #31
    
    * feat: load Category

---
+++ src/electron/database/models/category.schema.ts
@@ -0,0 +1,11 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Category
+{
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
