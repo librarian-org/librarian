4b7ef57b7eae22992c88dd82aecb704468474f4a
André Luiz Gava <andrelzgava@gmail.com>
2021-11-25 08:46:26 -0300

    feat: create model region #45 (#50)

--- src/electron/database/models/country.schema.ts
+++ src/electron/database/models/country.schema.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Region } from './region.schema';
 
 @Entity()
 export class Country
@@ -8,4 +9,7 @@ export class Country
 
   @Column()
   name: string;
+
+  @OneToMany(() => Region, region => region.country)
+  regions: Region[];
 }
