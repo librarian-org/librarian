0143deae0837d77ed5791c329c9eb5555cb01635
André Luiz Gava <andrelzgava@gmail.com>
2021-11-25 08:39:32 -0300

    feat: create model user #48 (#53)

---
+++ src/database/migration/1637784086085-create_user.ts
@@ -0,0 +1,62 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class createUser1637784086085 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "user",
+            columns: [
+                {
+                    name: "id", 
+                    type: "int",
+                    isPrimary: true
+                },
+                {
+                    name: "name",
+                    type: "varchar",
+                },
+                {
+                    name: "login",
+                    type: "varchar",
+                },
+                {
+                    name: "password",
+                    type: "varchar",
+                },
+                {
+                    name: "language",
+                    type: "varchar",
+                },
+                {
+                    name: "notes",
+                    type: "varchar",
+                },
+                {
+                    name: "document",
+                    type: "varchar",
+                },
+                {
+                    name: "user_type_id",
+                    type: "int",
+                },
+            ]
+        }), true)
+
+        await queryRunner.createForeignKey("user", new TableForeignKey({
+            columnNames: ["user_type_id"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "user_type",
+            onDelete: "CASCADE"
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        const table = await queryRunner.getTable("user");
+
+        const userTypeId = table.foreignKeys.find(fk => fk.columnNames.indexOf("user_type_id") !== -1);
+        await queryRunner.dropForeignKey("user", userTypeId);
+        
+        await queryRunner.dropTable("user");
+    }
+
+}
