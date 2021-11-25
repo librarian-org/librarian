03d54b61386e1a84c5cc834bc3956bbfd0cb4a15
André Luiz Gava <andrelzgava@gmail.com>
2021-11-25 08:39:32 -0300

    feat: create model user #48 (#53)

--- src/electron/database/models/user.schema.ts
+++ src/electron/database/models/user.schema.ts
@@ -1,11 +1,32 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { TypeUser } from './type_user.schema';
 
 @Entity()
-export class User
-{
+export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
+
+  @Column()
+  login: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  language: string;
+
+  @Column()
+  notes: string;
+
+  @Column()
+  document: string;
+
+  @Column()
+  user_type_id: number;
+
+  @ManyToOne(() => TypeUser, type => type.users)
+  type: TypeUser;
 }
