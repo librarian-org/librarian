a8f90651628c122a61c34bb7d81d20a894ab7b42
Danilo Lutz <danilolutz@protonmail.com>
2021-11-28 11:38:03 -0300

    feat: added tab navigation and shortcutcomponent - closes #79 #80

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,11 @@
 import React, { useCallback, useEffect } from 'react';
 import { DefaultTheme, ThemeProvider } from 'styled-components';
-import LanguageSelector from './components/I18n/LanguageSelector';
 import usePersistedState from './hooks/usePersistedState';
 
 import GlobalStyle from './styles/global';
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
-import Header from './components/Header';
 import Content from './components/Content';
-import Card from './components/Card';
-import Button from './components/Button';
-import Input from './components/Input';
-import { FiAlignJustify } from 'react-icons/fi';
 import Tabs from './components/Tabs';
 import { AppEvent } from '../common/AppEvent';
 
