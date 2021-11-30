05718a42f5d3dd103125ebc4312676dc496d0346
Danilo Lutz <danilolutz@protonmail.com>
2021-11-30 15:51:50 -0300

    feat: save selected language

--- src/electron/Menu.ts
+++ src/electron/Menu.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, Notification, shell } from 'electron';
+import { app, BrowserWindow, shell } from 'electron';
 import { i18n } from 'i18next';
 import path from 'path';
 import { AppEvent } from '../common/AppEvent';
