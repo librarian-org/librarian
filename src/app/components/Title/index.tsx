41c7ecf04f0ff2c562883a82e7a485bb121c943b
Danilo Lutz <danilolutz@protonmail.com>
2021-12-02 15:50:43 -0300

    feat: added category component closes #88

--- src/app/components/Title/index.tsx
+++ src/app/components/Title/index.tsx
@@ -1,11 +1,69 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import AuthorSelect from '../AuthorSelect';
+import Button from '../Button';
+import Input from '../Input';
+import PublisherSelect from '../PublisherSelect';
 
 import { Container } from './styles';
 
 const Title: React.FC = () => {
   return (
     <Container>
-      Title
+      <Input
+        type="text"
+        name="name"
+        label="Name"
+        placeholder="Type the name of the Title"
+      />
+
+      <Input
+        type="text"
+        name="ISBN"
+        label="ISBN"
+        placeholder="Type the ISBN of the Title"
+      />
+
+      <Input
+        type="text"
+        name="classification"
+        label="Classification"
+        placeholder="Type the classification"
+      />
+
+      <Input
+        type="text"
+        name="edition"
+        label="Edition"
+        placeholder="Type the edition"
+      />
+
+      <Input
+        type="date"
+        name="edition_date"
+        label="Edition Date"
+        placeholder="Type the edition date"
+        alt="Type the edition date"
+      />
+
+      <PublisherSelect />
+      <Button color="primary">
+        <FiPlus size={20} />
+      </Button>
+
+      <AuthorSelect />
+      <Button color="primary">
+        <FiPlus size={20} />
+      </Button>
+
+      {/* <Category />
+      <Button color="primary">
+        <FiPlus size={20} />
+      </Button> */}
+
+      <Button color="primary">
+        Salvar
+      </Button>
     </Container>
   );
 };
