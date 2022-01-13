68d7056c9128ff828a5c4c73325c215349585fdf
Danilo Lutz <danilolutz@protonmail.com>
2022-01-13 15:34:56 -0300

    feat: added table component

---
+++ src/app/components/Table/TablePagination/index.tsx
@@ -0,0 +1,124 @@
+import React, {
+  ChangeEvent,
+  PropsWithChildren,
+  ReactElement,
+  useCallback,
+  useEffect,
+} from 'react';
+import {
+  MdFirstPage,
+  MdLastPage,
+  MdNavigateBefore,
+  MdNavigateNext,
+} from 'react-icons/md';
+import { TableInstance } from 'react-table';
+
+import { Container } from './styles';
+
+const rowsPerPageOptions = [10, 20, 30, 40, 50];
+
+export interface Search {
+  pageSize: number;
+  pageIndex: number;
+}
+
+export function TablePagination<T extends Record<string, unknown>>({
+  instance,
+  fetchData,
+  setRowsPerPage,
+}: PropsWithChildren<{
+  instance: TableInstance<T>;
+  fetchData(search: Search): Promise<void>;
+  setRowsPerPage(size: number): void;
+}>): ReactElement | null {
+  const {
+    state: { pageIndex, pageSize },
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    canPreviousPage,
+    canNextPage,
+    pageCount,
+  } = instance;
+
+  useEffect(() => {
+    fetchData({ pageIndex, pageSize });
+  }, [fetchData, pageIndex, pageSize]);
+
+  const pageSizeChange = useCallback(
+    (event: ChangeEvent<HTMLSelectElement>) => {
+      setPageSize(Number(event.target.value));
+      setRowsPerPage(Number(event.target.value));
+    },
+    [setPageSize, setRowsPerPage],
+  );
+
+  const gotoPageChange = useCallback(
+    (event: ChangeEvent<HTMLSelectElement>) => {
+      gotoPage(Number(event.target.value));
+    },
+    [gotoPage],
+  );
+
+  return (
+    <Container>
+      <span>
+        Página
+        <select
+          value={pageIndex}
+          onBlur={gotoPageChange}
+          onChange={gotoPageChange}
+        >
+          {[...Array(pageCount)].map((_, i) => (
+            <option key={i.toString()} value={i}>
+              {i + 1}
+            </option>
+          ))}
+        </select>
+        de
+        <strong>
+          &nbsp;
+          {pageCount}
+        </strong>
+      </span>
+      <span />
+      &nbsp;&bull;&nbsp;Itens por página:&nbsp;
+      <select
+        value={pageSize}
+        onBlur={pageSizeChange}
+        onChange={pageSizeChange}
+      >
+        {rowsPerPageOptions.map(rowsPerPage => (
+          <option key={rowsPerPage} value={rowsPerPage}>
+            {rowsPerPage}
+          </option>
+        ))}
+      </select>
+      <button
+        type="button"
+        onClick={() => gotoPage(0)}
+        disabled={!canPreviousPage}
+      >
+        <MdFirstPage size={20} />
+      </button>
+      <button
+        type="button"
+        onClick={() => previousPage()}
+        disabled={!canPreviousPage}
+      >
+        <MdNavigateBefore size={20} />
+      </button>
+      <button type="button" onClick={() => nextPage()} disabled={!canNextPage}>
+        <MdNavigateNext size={20} />
+      </button>
+      <button
+        type="button"
+        onClick={() => gotoPage(pageCount - 1)}
+        disabled={!canNextPage}
+      >
+        <MdLastPage size={20} />
+      </button>
+    </Container>
+  );
+}
