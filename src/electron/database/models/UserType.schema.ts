18865101c895f4b5ccc4de095dbd2ca98f8a1492
Danilo Lutz <danilolutz@protonmail.com>
2022-01-22 14:41:02 -0300

    feat: select input component (#135)

--- src/electron/database/models/UserType.schema.ts
+++ src/electron/database/models/UserType.schema.ts
@@ -2,15 +2,13 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { User } from './User.schema';
 
 @Entity()
-export class UserType
-{
+export class UserType {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
 
-  @OneToMany(() => User, user => user.type)
+  @OneToMany(() => User, (user) => user.userType)
   users: User[];
-
 }
