6091d004d1ad69330faee1b8aada931b5a33c4a4
Danilo Lutz <danilolutz@protonmail.com>
2022-02-14 10:57:25 -0300

    feat: person list

--- src/app/components/Person/List/index.tsx
+++ src/app/components/Person/List/index.tsx
@@ -1,57 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import Button from '../../Button';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-import { Container } from './styles';
-import { trigger } from '../../../util/EventHandler';
-import { AppEvent } from '../../../../common/AppEvent';
+import { Container, ColumWrapper } from './styles';
+import { PaginatedSearch, Table } from '../../../components/Table';
 import { Person } from '../Person';
+import i18n from '../../../i18n';
+import { Cell, Column } from 'react-table';
+import { Search } from '../../Table/TablePagination';
+import { useToast } from '../../../hooks/toast';
+import { AppEvent } from '../../../../common/AppEvent';
+import { trigger } from '../../../util/EventHandler';
+import { Actions } from '../../../../common/Actions';
+import { FaPen } from 'react-icons/fa';
 
-const ListPerson: React.FC = () => {
-  const [items, setItems] = useState([]);
+const PersonList: React.FC = () => {
+  const { addToast } = useToast();
+  const [list, setList] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [pageCount, setPageCount] = React.useState(0);
 
   useEffect(() => {
-    const result = window.api.sendSync('list', {
+    try {
+    setLoading(true);
+    const response = window.api.sendSync('listPerson', {
       entity: 'User',
-    }) as Person[];
-    setItems(result);
-  }, []);
+      value: {
+        where: null,
+        pageStart: 0,
+        pageSize: rowsPerPage,
+      },
+    }) as PaginatedSearch<Person>;
+    setList(response.data);
+    setLoading(false);
+  } catch (err) {
+    console.error(err);
+  }
+
+  }, [rowsPerPage]);
 
   const handleUpdate = (item: Person): void => {
-    trigger(AppEvent.personTab, { action: 'update', value: item });
+    trigger(AppEvent.personTab, { action: Actions.update, value: item});
   };
 
-  const handleRead = (item: Person): void => {
-    trigger(AppEvent.personTab, { action: 'read', value: item });
+  const handleRowClick = (item: Person) => {
+    trigger(AppEvent.personTab, { action: Actions.read, value: item });
   };
+
+  const columns: Array<Column<Person>> = useMemo(
+    () => [
+      {
+        Header: i18n.t('person.name'),
+        accessor: 'name',
+      },
+      {
+        Header: i18n.t('person.userType'),
+        id: 'userType',
+        Cell: (b: Cell<Person>) => {
+          return <>{b.row.original.userType.name}</>;
+        },
+      },
+      {
+        Header: () => null,
+        id: 'edit',
+        Cell: (row: Cell<Person>) => {
+          return (
+          <ColumWrapper>
+            <FaPen size={20} title={i18n.t('person.edit')} onClick={(event) => { event.stopPropagation(); handleUpdate(row.row.original)}} />
+          </ColumWrapper>)
+        }
+      }
+    ],
+    [],
+  );
+
+
+  const handleSubmit = useCallback(
+    async ({ pageIndex = 0 }: Search) => {
+      try {
+        setLoading(true);
+        const response = window.api.sendSync('listPerson', {
+          entity: 'User',
+          value: {
+            where: null,
+            pageStart: rowsPerPage * pageIndex,
+            pageSize: rowsPerPage,
+          },
+        }) as PaginatedSearch<Person>;
+        setList(response.data);
+        setPageCount(Math.ceil(response.count / rowsPerPage));
+
+        setLoading(false);
+
+        setList(response.data);
+      } catch (err) {
+        addToast({
+          type: 'error',
+          title: 'Erro ao listar os endereços',
+          description:
+            'Ocorreu um erro ao listar os endereços, tente novamente.',
+        });
+      }
+    },
+    [addToast, rowsPerPage],
+  );
 
   return (
     <Container>
-      <>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>ISBN</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.name}</td>
-                  <td>
-                    <Button onClick={() => handleRead(item)}>Ver</Button>
-                    <Button onClick={() => handleUpdate(item)}>Editar</Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
+      <Table<Person>
+        name="title"
+        columns={columns}
+        onRowClick={handleRowClick}
+        data={list}
+        fetchData={handleSubmit}
+        loading={loading}
+        pageCount={pageCount}
+        setRowsPerPage={setRowsPerPage}
+      />
     </Container>
   );
 };
 
-export default ListPerson;
+export default PersonList;
