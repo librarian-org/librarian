6933e44bfade9a71b65dbb8542c9ffa2290ce184
Danilo Lutz <danilolutz@protonmail.com>
2022-02-14 11:09:41 -0300

    feat: person list (#142)

--- src/app/components/Person/List/styles.ts
+++ src/app/components/Person/List/styles.ts
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-grow: 1;
+`;
 
+
+export const ColumWrapper = styled.div`
+  display: flex;
+  justify-content: end;
+  padding-right: 24px;
 `;
