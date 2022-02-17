06a3247ee679e3b89241a9cf36a8518ceec0330a
Danilo Lutz <danilolutz@protonmail.com>
2022-02-17 14:51:48 -0300

    feat: isbn validation (#149)

--- src/app/hooks/useBarcode.ts
+++ src/app/hooks/useBarcode.ts
@@ -30,6 +30,20 @@ export interface BarcodeProps {
   options?: Options;
 }
 
+const checksum = (number: string): number => {
+  const res = number
+    .substring(0, 12)
+    .split('')
+    .map((n) => +n)
+    .reduce((sum, a, idx) => (idx % 2 ? sum + a * 3 : sum + a), 0);
+
+  return (10 - (res % 10)) % 10;
+};
+
+export function isValidEAN13(value: string): boolean {
+  return value.search(/^[0-9]{13}$/) !== -1 && +value[12] === checksum(value);
+}
+
 export function useBarcode({ ...props }: BarcodeProps): any {
   const inputRef = useRef();
   const { value, options } = props;
