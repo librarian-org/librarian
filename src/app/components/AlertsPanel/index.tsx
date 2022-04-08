1e9f89876bbdbcb5689867229c4250d9f62931a1
Danilo Lutz <danilolutz@protonmail.com>
2022-04-08 14:58:56 -0300

    feat: added alerts counter and improved alerts list (#165)
    * feat: added alerts counter and improved alerts list

--- src/app/components/AlertsPanel/index.tsx
+++ src/app/components/AlertsPanel/index.tsx
@@ -1,15 +1,18 @@
 import React, { useCallback } from 'react';
-import { FiX } from 'react-icons/fi';
+import { FiChevronDown, FiX } from 'react-icons/fi';
+import { CgPlayListRemove } from 'react-icons/cg';
 import { ToastMessage, useToast } from '../../hooks/toast';
+import i18n from '../../i18n';
 import Translator from '../I18n/Translator';
 
-import { AlertItem, AlertList, Container, Clear } from './style';
+import { AlertItem, AlertList, Container, Clear, AlertsActions } from './style';
 
 interface AlertPanelProps {
   alerts: ToastMessage[];
+  handleAlertPanelClick(): void;
 }
 
-const AlertsPanel: React.FC<AlertPanelProps> = ({ alerts }) => {
+const AlertsPanel: React.FC<AlertPanelProps> = ({ alerts, handleAlertPanelClick }) => {
   const { removeAlerts, removeAllAlerts } = useToast();
 
   const handleRemoveAlert = useCallback(
@@ -25,10 +28,14 @@ const AlertsPanel: React.FC<AlertPanelProps> = ({ alerts }) => {
 
   return (
     <Container>
-      <Clear onClick={handleRemoveAllAlert}>
-        <FiX size={10}  />
-        Limpar tudo
-      </Clear>
+      <AlertsActions>
+        <Clear onClick={handleRemoveAllAlert}>
+          <CgPlayListRemove size={20} title={i18n.t('notifications.clearAll')} />
+        </Clear>
+        <Clear onClick={handleAlertPanelClick}>
+          <FiChevronDown size={20} title={i18n.t('notifications.hide')} />
+        </Clear>
+      </AlertsActions>
       <AlertList>
         {alerts && alerts.length === 0 && (
           <AlertItem>
